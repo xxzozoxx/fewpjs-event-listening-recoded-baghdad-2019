07cacdcde729cb56ea6fcca1fe75709b276d9ce7
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-15 15:53:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+ const input = document.getElementById('input');
+input.addEventListener('click', function addingEventListener (event) {
+  alert('I was clicked!');
+});
 }
